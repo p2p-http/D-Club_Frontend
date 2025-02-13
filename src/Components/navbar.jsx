13752bb429db3009b322670a573555977a7a437f
Onkar Dhotarkar <onkardhotarkar@gmail.com>
2025-02-13 13:35:49 +0530

    feat:navbar fixed,

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -3,16 +3,19 @@ import login from '../assets/login.png'
 import { useNavigate, useNavigation } from 'react-router-dom'
 
 
+// bg-[#312F2F]  shadow-gray-700 drop-shadow-xl
+
+
 const Navbar = () => {
   const navigate = useNavigate()
 
     return (
-        <div className="fixed shadow-[5px_5px_15px_rgba(0,0,0,0.3)] shadow-gray-700 drop-shadow-xl top-0 left-0 w-full bg-[#312F2F] text-white flex items-center justify-between px-16  py-2 z-50">
+        <div className="fixed shadow-[5px_5px_15px_rgba(0,0,0,0.3)] bg-black  shadow-gray-700 drop-shadow-xl  top-0 left-0 w-full  text-white flex items-center justify-between px-16  py-2 z-50">
             <div className="flex flex-col cursor-pointer">
-                <span onClick={()=>navigate('/')} className="logo text-4xl">D-<span className="text-[#FFD700]">Club</span></span>
-                {/* <span className="font-dmMono text-[7px]  ">
-                    Discover Dance Duo
-                </span> */}
+                <span onClick={()=>navigate('/')} className="logo text-3xl">D-<span className="text-[#FFD700] font-extrabold">Club</span></span>
+                <span className="font-Poppins text-[8.5px] tracking-wider   ">
+                    Discover. Dance. Duo.
+                </span>
             </div>
             <div className="flex items-center justify-center space-x-6 text-lg font-[Poppins]">
                 <span className="hover:text-[#ff9684] cursor-pointer hover:scale-105 duration-500  ">we</span>
