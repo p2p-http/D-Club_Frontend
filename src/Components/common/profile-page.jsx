6897e4aa6efd5d8475283a7d57c27ae4e5e71638
Onkar Dhotarkar <onkardhotarkar@gmail.com>
2025-02-23 12:41:01 +0530

    feat: desing to logout page

--- src/Components/common/profile-page.jsx
+++ src/Components/common/profile-page.jsx
@@ -17,7 +17,7 @@ const ProfilePage = () => {
       <h1 className="text-2xl font-bold mb-4">Profile Page</h1>
       <button
         onClick={handleLogout}
-        className="bg-red-500 px-4 py-2 rounded-md text-white hover:bg-red-700 transition"
+        className="bg-[#FFD700] px-6 py-2 rounded-xl text-gray-600 hover:bg-yellow-600 transition font-semibold"
       >
         Logout
       </button>
