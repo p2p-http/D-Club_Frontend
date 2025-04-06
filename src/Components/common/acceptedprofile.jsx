eac9ceeed9fdc1b5353694a4a7c86c9ffaeaa74f
Onkar Dhotarkar <onkardhotarkar@gmail.com>
2025-04-06 11:13:02 +0530

    update accepted profile

--- src/Components/common/acceptedprofile.jsx
+++ src/Components/common/acceptedprofile.jsx
@@ -30,7 +30,7 @@ const Acceptedprofile = () => {
     if (isLoading) {
         return (
             <div className="flex justify-center items-center pt-44 min-h-screen">
-                <svg className="animate-spin h-8 w-8 text-[#4CAF50]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <svg className="animate-spin h-8 w-8 text-[#FFD700]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                     <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                     <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                 </svg>
@@ -65,7 +65,7 @@ const Acceptedprofile = () => {
                             {user.fullName}
                         </h2>
                         <p className="text-[#868181] text-sm sm:text-base">
-                            {user.bio}
+                            {user.bio || 'User has not updated bio!'}
                         </p>
                         <div className="gender-age text-[#868181] flex gap-3 pt-2 justify-center sm:justify-start">
                             <p>{user.dateOfBirth
